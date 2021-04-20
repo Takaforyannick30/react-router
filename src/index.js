7e4c374075f81224a44e77d1b50f250e0dfac529
Takaforyannick30 <takaforyannick30@gmail.com>
2021-04-20 23:01:20 +0200

    Render Router in index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/contact">Contact-me</Link></li>
+      </ul>
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route  path="/contact" component={Contact} />
+
+
+    </div>
+  </Router>
+  ,
   document.getElementById('root')
 );
 
